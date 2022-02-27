4b5ffaf514442a4e04bc3f0a9ff5ccc4eddf17cd
itunuelijah <itunuelijah@gmail.com>
2022-02-27 15:26:05 +0100

    count vowel

---
+++ src/CountVowelFromName.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CountVowelFromName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int numberOfCharacters;
+        int vowelCount = 0;
+
+    }
+}
