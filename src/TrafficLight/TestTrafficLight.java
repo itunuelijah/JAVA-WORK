c8ffa3be5e128e81727da5ba25187ad645cffb7e
itunuelijah <itunuelijah@gmail.com>
2022-03-12 07:20:22 +0100

    traffic light test

---
+++ src/TrafficLight/TestTrafficLight.java
@@ -0,0 +1,9 @@
+package TrafficLight;
+
+public class TestTrafficLight {
+    public static void main(String[] args) {
+         TrafficLight light = TrafficLight.RED;
+        System.out.println(light.getDescription());
+    }
+
+}
