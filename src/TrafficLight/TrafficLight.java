a4f0d69823c9d600dc1ca36f1147d635a599ef08
itunuelijah <itunuelijah@gmail.com>
2022-03-12 07:20:47 +0100

    traffic light

---
+++ src/TrafficLight/TrafficLight.java
@@ -0,0 +1,17 @@
+package TrafficLight;
+
+public enum TrafficLight {
+    RED ("Please stop"),
+    GREEN ("Please go"),
+    YELLOW ("Please caution");
+
+    private String description;
+
+    private TrafficLight(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+  }
+
