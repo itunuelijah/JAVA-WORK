e53381d98d2d6c279e07f0bb796c6e27d9c02b41
itunuelijah <itunuelijah@gmail.com>
2022-05-09 12:03:26 +0100

    leetcode

--- src/Find123pattern.java
+++ src/Find123pattern.java
@@ -2,20 +2,18 @@ public class Find123pattern {
 
 
 //    public boolean find123pattern(int[] nums) {
-public static void main(String[] args) {
+public static boolean main(String[] args) {
     int[] nums = {1, 3, 2};
     for (int i = 0; i < nums.length - 2; i++) {
         for (int j = 0; j < nums.length - 1; j++) {
             for (int k = 0; k < nums.length; k++) {
                 if (nums[k] > nums[i] && nums[j] > nums[k])
-                    System.out.println("true");
-
-                    System.out.println("false");
+                    return true;
             }
         }
     }
 
-
+    return false;
 }
 }
    
