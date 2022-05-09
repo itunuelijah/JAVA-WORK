cbc3ca5e5e9e241741d0a6455b7488f9b45ac7ac
itunuelijah <itunuelijah@gmail.com>
2022-05-09 12:05:36 +0100

    leetcode

--- src/Find123pattern.java
+++ src/Find123pattern.java
@@ -1,9 +1,8 @@
 public class Find123pattern {
 
 
-//    public boolean find123pattern(int[] nums) {
-public static boolean main(String[] args) {
-    int[] nums = {1, 3, 2};
+    public boolean find123pattern(int[] nums) {
+    nums = new int[]{1, 3, 2};
     for (int i = 0; i < nums.length - 2; i++) {
         for (int j = 0; j < nums.length - 1; j++) {
             for (int k = 0; k < nums.length; k++) {
@@ -16,4 +15,5 @@ public static boolean main(String[] args) {
     return false;
 }
 }
-   
+
+
